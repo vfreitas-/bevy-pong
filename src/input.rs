612a32bfd4340df8799577afc66a8d9adaad5a0d
Vitor Freitas <vitor_freitas_b@hotmail.com>
2022-02-22 14:30:09 -0300

    fix(input): fix state transition

--- src/input.rs
+++ src/input.rs
@@ -14,7 +14,7 @@ fn input (
   mut state: ResMut<State<GameState>>,
   keyboard_input: Res<Input<KeyCode>>,
 ) {
-  if keyboard_input.pressed(KeyCode::Return) {
+  if keyboard_input.just_released(KeyCode::Return) {
     if &GameState::Start == state.current() {
       state.set(GameState::Playing).unwrap()
     } else if &GameState::Playing == state.current() {
